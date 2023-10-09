cc2686da7c6b06ca5b4768a6170720809da7d97a
Ajayi Olamide Daniel <prodbyola@Ajayis-MacBook-Pro.local>
2023-10-09 11:09:43 +0100

    Updated ChatListSscreen.

--- app/src/main/java/com/dududaa/nadchat/ui/screens/ChatList.kt
+++ app/src/main/java/com/dududaa/nadchat/ui/screens/ChatList.kt
@@ -1,13 +1,16 @@
 package com.dududaa.nadchat.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dududaa.nadchat.ui.components.ChatListComponent
+import com.dududaa.nadchat.ui.components.SearchField
 
 @Composable
 fun ChatListScreen() {
@@ -16,6 +19,9 @@ fun ChatListScreen() {
             .fillMaxWidth()
             .padding(horizontal = 16.dp)
     ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        SearchField()
+        Spacer(modifier = Modifier.height(16.dp))
         ChatListComponent()
     }
 }
